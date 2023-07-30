865f78f19a1849346be4b718c61bcd84a33ad1fc
Ivaneskins <weina2004@mail.ru>
2023-07-31 00:52:39 +0500

    Done

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,6 +1,21 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+
+    private Deth8 deth;
+
+    public Deth8 getDeth() {
+        return deth;
+    }
+
+    @Autowired
+    public void setDeth(Deth8 deth) {
+        this.deth = deth;
+    }
 
     @Override
     public String toString() {
